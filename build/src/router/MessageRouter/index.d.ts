8f8fc32d3c6a3b46f73318ab881a1103becdf04a
narcis25 <narcis25@gmail.com>
2024-01-16 16:10:22 +0900

    delete

--- build/src/router/MessageRouter/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const MessageRouter: import("express-serve-static-core").Router;
--- build/src/router/MessageRouter/post.d.ts
+++
@@ -1,2 +0,0 @@
-import { Request, Response } from 'express';
-export declare const postMessage: (req: Request, res: Response) => Promise<void>;
