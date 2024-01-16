4bb8909af03ae2dd81c859ceb049ace56f5e6c1f
narcis25 <narcis25@gmail.com>
2024-01-16 13:57:10 +0900

    post test

--- src/router/MessageRouter/post.ts
+++ src/router/MessageRouter/post.ts
@@ -2,9 +2,10 @@ import {firestore} from 'firebase-admin';
 import {Request, Response} from 'express';
 
 export const postMessage = async (req: Request, res: Response) => {
-  const {message} = req.query;
+  res.send('TTTTTT');
+  /*const {message} = req.query;
 
   const ref = firestore().collection('message');
   ref.add({message});
-  res.status(200).send('message added');
+  res.status(200).send('message added');*/
 };
